9dcfe3b77caf042615bb90cc176327a6cef74960
aybid <dbyapoudel22@gmail.com>
2022-06-09 18:00:44 +0545

    day1Commit

--- src/firstProjetct/header/Header.js
+++ src/firstProjetct/header/Header.js
@@ -6,7 +6,7 @@ import './header.css';
 const Header = () => {
   return (
    <div className="mainHeader">
-   <div className="logo">Neutroline</div>
+   <div className="logo">Neutro</div>
     <nav>
     <input type="checkbox" id="check"/>
      <label htmlFor="check" className="checkBtn">
